ecc5e62c56f51608f06796c02b109a6fac5acd19
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-10 11:14:49 +0900

    first commit後、再チェック修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :move_to_index, only: [:edit]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:edit, :show, :update, :destroy]
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user).order("created_at DESC")
@@ -47,12 +47,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def set_tweet
+  def set_prototype
     @prototype = Prototype.find(params[:id])
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
